10a16b26555b5ba500351542a6c518bce75e9f71
rahuldcoder <rahulthakur02k@gmail.com>
2018-06-13 15:05:46 -0600

    removed redundant space

--- pros_cons.py
+++ pros_cons.py
@@ -3,16 +3,15 @@ from bs4 import BeautifulSoup
 import pprint
 
 pp = pprint.PrettyPrinter(indent=4)
-url = 'https://www.oyster.com/maldives/hotels/kuredu-island-resort-and-spa'
+# url = 'https://www.oyster.com/maldives/hotels/kuredu-island-resort-and-spa'
+
+print('Enter the URL of the OYESTER Website')
+url = input()
 page = requests.get(url)
 soup = BeautifulSoup(page.text,'html.parser')
 
 section_pros = soup.find('section',{'class' : 'pros'}).ul.contents
 section_cons = soup.find('section',{'class' : 'cons'}).ul.contents
-
-
-
-
 
 new_line_char = '\n'
 
@@ -25,14 +24,12 @@ file_name = url[39:]
 with open(file_name+'-PROS.txt','w') as file_handle:
     
     file_handle.write('PROS'+'\n')
-
     for html_item in section_pros:
         file_handle.writelines(html_item.text+'\n')
 
 with open(file_name+'-CONS.txt','w') as file_handle:
 
     file_handle.write('CONS'+'\n')
-
     for html_item in section_cons:
         file_handle.writelines(html_item.text+'\n')
 
